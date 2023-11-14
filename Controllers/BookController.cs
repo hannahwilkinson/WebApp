aa647ffcbc6273f9eb507d1d3bda14fc21d363bd
Hannah Wilkinson <100706760+hannahwilkinson@users.noreply.github.com>
2023-11-14 03:37:40 -0500

    Add files via upload

---
+++ Controllers/BookController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _450MIST.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace _450MIST.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace _450MIST.Controllers
+
+    //Controller (base class)
+    public class BookController : Controller
+    {
+        private BooksDBContext _context; //will hold a reference to the database object
+
+        public BookController(BooksDBContext context)
+        {
+            _context = context; //passes the reference to the database in the _context variable
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index() //method that allows us to read the information from the database
+        {
+            List<Book> booksList = _context.books.ToList(); //gets a list of all the books in the books table
+
+            //??passing booksList to the view
+            return View(booksList);
+
+            //pass the data to the view that will display the data
+        }
+
+        //HTTP get request - getting the form
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //HTTP post request - post the data
+        //get and save the data to the database
+        //overloaded method
+        //Book -
+        //books - is the table reference (BooksDBContext)
+
+        [HttpPost]
+        public IActionResult Create(Book bookObj)
+        {
+            _context.books.Add(bookObj); //getting ready to add the book
+            _context.SaveChanges(); //this is when we actually add the book to the database
+
+            return RedirectToAction("Index", "Book");
+        }
+    }
---
+++ Data/BooksDBContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using _450MIST.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _450MIST.Data
+{
+	public class BooksDBContext : DbContext
+	{
+		public BooksDBContext(DbContextOptions<BooksDBContext> options) : base(options)
+		{
+
+		}
+
+		//add a table to the database where every row is a book
+		public DbSet<Book> books { get; set; }
+
+		//put in seed data (mock data)
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			//HasData method - add data to our entity "books"
+			modelBuilder.Entity<Book>().HasData(
+
+				//BookID needs to be unique
+				new Book
+				{
+					BookID = 1,
+					Title = "C# for Beginners",
+					Description = "This has no description",
+					Author = "Salman Nazir",
+					Genre = "Coding",
+					Price = 19.99m,
+					DatePublished = new DateTime(2023, 10, 20)
+				},
+
+				new Book
+				{
+					BookID = 2,
+					Title = "Advanced C#",
+					Description = "This has no description",
+					Author = "Adman Freeman",
+					Genre = "Coding",
+					Price = 59.99m,
+					DatePublished = new DateTime(2022, 10, 20)
+				},
+
+				new Book
+				{
+					BookID = 3,
+					Title = "HTML for Beginners",
+					Description = "This has no description",
+					Author = "Morgan Freeman",
+					Genre = "Coding",
+					Price = 15.99m,
+					DatePublished = new DateTime(2020, 10, 20)
+				});
+		}
+	}
+}
